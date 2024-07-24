8412f4f3156a204ed4b90650772bf92ac894562f
Anthony Nolan <anthonynolan@gmail.com>
2024-07-24 16:36:02 +0100

    blog entry

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
+git add .
+git commit -m 'blog entry'
+git push
 aws s3 cp ./output/ s3://blog.cathalanddad.com/ --recursive
 aws cloudfront create-invalidation --distribution-id E1PGDI3DG3VVR3 --paths "/*"
