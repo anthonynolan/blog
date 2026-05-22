004ed9190622505f2cec5b661c8a9167000373b1
Anthony Nolan <anthonynolan@gmail.com>
2026-05-22 10:04:04 +0100

    2026-05-22 10:04:04 +0100

--- deploy.sh
+++ deploy.sh
@@ -4,26 +4,15 @@ set -e
 COMMIT_MESSAGE="$*"
 
 if [ -z "$COMMIT_MESSAGE" ]; then
-  changed_posts=$(git status --porcelain -- content/*.md | awk '{print $2}')
-  post_count=$(echo "$changed_posts" | sed '/^$/d' | wc -l)
-
-  if [ "$post_count" -ne 1 ]; then
-    echo "Usage: $0 \"Commit message\""
-    echo "Or change exactly one content/*.md post so its Title can be used."
-    exit 1
-  fi
-
-  changed_post=$(echo "$changed_posts" | sed '/^$/d')
-  COMMIT_MESSAGE=$(sed -n 's/^Title:[[:space:]]*//p' "$changed_post" | head -n 1)
-
-  if [ -z "$COMMIT_MESSAGE" ]; then
-    echo "Could not find a Title field in $changed_post"
-    exit 1
-  fi
+  COMMIT_MESSAGE=$(date '+%Y-%m-%d %H:%M:%S %z')
 fi
 
 git add .
-git commit -m "$COMMIT_MESSAGE"
+if git diff --cached --quiet; then
+  echo "No changes to commit."
+else
+  git commit -m "$COMMIT_MESSAGE"
+fi
 git push
 aws s3 sync ./output/ s3://blog.cathalanddad.com/ --size-only
 aws cloudfront create-invalidation --distribution-id E1PGDI3DG3VVR3 --paths "/*"
